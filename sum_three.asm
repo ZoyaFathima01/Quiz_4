736a49a934527710e8e3533b815ba9b8d1f708b8
ZoyaFathima01 <zoyaafathimaa001@gmail.com>
2025-08-12 22:52:44 -0700

    Update sum_three.asm

--- sum_three.asm
+++ sum_three.asm
@@ -1,45 +1,32 @@
-section .data
-    x dq 10         ; First integer
-    y dq 15         ; Second integer
-    z dq 5          ; Third integer
-
 section .text
     global _start
 
 _start:
-    ; Load values of x, y, z into registers
-    mov rdi, [x]     ; First argument
-    mov rsi, [y]     ; Second argument
-    mov rdx, [z]     ; Third argument
+    mov eax, 7
+    mov ebx, 12
+    mov ecx, 5
 
-    ; Call the add_three function
+    push ecx
+    push ebx
+    push eax
+
     call add_three
 
-    ; Return result in rax to shell (must be in 0-255 range for shell echo $? to work)
-    ; We'll store it in rax already, but make sure to convert to int and exit cleanly
+    mov [result], eax
 
-    ; Exit system call
-    mov rdi, rax     ; move result to rdi as exit code
-    mov rax, 60      ; syscall: exit
-    syscall
+    mov eax, 1
+    int 0x80
 
-; -------- Function: add_three -------------
-; Arguments:
-;   rdi = x
-;   rsi = y
-;   rdx = z
-; Returns:
-;   rax = result = x + y + z
 add_three:
-    push rbp
-    mov rbp, rsp     ; Set up stack frame (not strictly required, but clean)
-    
-    ; Add the three numbers
-    mov rax, rdi
-    add rax, rsi
-    add rax, rdx     ; rax = x + y + z
-    
-    ; Deallocate function stack (clean exit)
-    mov rsp, rbp
-    pop rbp
+    push ebp
+    mov ebp, esp
+
+    mov eax, [ebp+8]
+    add eax, [ebp+12]
+    add eax, [ebp+16]
+
+    pop ebp
     ret
+
+section .bss
+    result resd 1
